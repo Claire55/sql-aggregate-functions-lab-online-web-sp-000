bd3a72a4107c0de4ccbeb3ece12a04defa7283dc
Claire Bullock <bullockcm13@gmail.com>
2021-02-16 17:07:58 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(*) FROM students WHILE tardies > 0"
+  "SELECT COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
